edfd1a8c7edd7c309ef0c558b93be1785b80155c
Pepe Armenta <jab_macmini@JABs-Mac-mini.local>
2026-09-05 22:05:26 -0600

    wip

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 echo "setup scripts initialized"
+set -euo pipefail
 
-echo "set -o vi" >> ~/.bashrc
+if command -v chezmoi >/dev/null; then
+  chezmoi init --apply --source $PWD
+  exit 0
+fi
+
+if command -v mise >/dev/null; then
+  mise exec chezmoi -- chezmoi init --apply --source $PWD
+  exit 0
+fi
+
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply --source $PWD
+exit 0
