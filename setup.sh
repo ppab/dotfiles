8c5c92cdb1f2e94fee62ef590153a6d791e57d42
ppab <ppab@fedora>
2026-09-07 04:17:35 +0000

    wip

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 echo "setup scripts initializing-->>>"
 set -euo pipefail
 
+if command -v zsh >/dev/null; then
+    sudo chsh -s $(command -v zsh) $USER
+fi
+
 if command -v chezmoi >/dev/null; then
   chezmoi init --apply --source $PWD
   exit 0
